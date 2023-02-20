4a12b4e17bb8577cb587efeb3fa4f157b9c99703
Tatsuki Kurosawa <kuro96831@gmail.com>
2023-02-21 01:22:57 +0900

    second_commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,11 @@
 /*   By: kurosawaitsuki <kurosawaitsuki@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 16:57:53 by kurosawaits       #+#    #+#             */
-/*   Updated: 2023/02/21 01:15:46 by kurosawaits      ###   ########.fr       */
+/*   Updated: 2023/02/21 01:21:24 by kurosawaits      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <fcntl.h>
-#include <stdio.h>
 
 static char	*join(char *str, char *read_str, ssize_t num_of_byte)
 {
@@ -112,28 +110,3 @@ char	*get_next_line(int fd)
 	str = getafternewline(str);
 	return (return_ptr);
 }
-
-// int	main(void)
-// {
-// 	int		fd;
-// 	char	*line;
-// 	size_t	i;
-
-// 	i = 0;
-// 	fd = open("./test.txt", O_RDONLY);
-// 	while (i < 6)
-// 	{
-// 		// printf("%lu回目\n", i + 1);
-// 		line = get_next_line(fd);
-// 		printf("203|%s|\n", line);
-// 		i ++;
-// 	}
-// 	return (0);
-// }
-
-// gcc -Wall -Wextra -Werror get_next_line.c get_next_line_utils.c
-// -g -fsanitize=address -fsanitize=undefined
-// gcc -Wall -Wextra -Werror -D BUFFER_SIZE=42 get_next_line.c 
-//get_next_line_utils.c -g -fsanitize=address -fsanitize=undefined
-// gcc -Wall -Wextra -Werror -D BUFFER_SIZE=1 get_next_line.c 
-//get_next_line_utils.c -g -fsanitize=address -fsanitize=undefined
---
+++ main.c
@@ -0,0 +1,27 @@
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+	size_t	i;
+
+	i = 0;
+	fd = open("./test.txt", O_RDONLY);
+	while (i < 6)
+	{
+		line = get_next_line(fd);
+		printf("203|%s|\n", line);
+		i ++;
+	}
+	return (0);
+}
+
+// gcc -Wall -Wextra -Werror main.c get_next_line.c get_next_line_utils.c
+// -g -fsanitize=address -fsanitize=undefined
+// gcc -Wall -Wextra -Werror -D BUFFER_SIZE=42 main.c get_next_line.c 
+//get_next_line_utils.c -g -fsanitize=address -fsanitize=undefined
+// gcc -Wall -Wextra -Werror -D BUFFER_SIZE=1 main.c get_next_line.c 
+//get_next_line_utils.c -g -fsanitize=address -fsanitize=undefined
